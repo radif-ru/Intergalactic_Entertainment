096f7edb7b59737275e4be9f512c9fa39d515c40
Ruslan Huzin <huzin.ruslan@mail.ru>
2021-04-04 20:01:48 +0500

    'reg_login_edit'

--- intergalactic/intergalactic/urls.py
+++ intergalactic/intergalactic/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import mainapp.views as mainapp
 
 from django.conf import settings
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
+
+    path('auth/', include('authapp.urls', namespace='auth')),
 
     path('admin/', admin.site.urls),
 ]
