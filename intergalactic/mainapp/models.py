2e894b257fd0402944f5629727122ce8fca2de25
Dementev Oleg <demeoleg@mail.ru>
2021-04-04 19:44:35 +0300

    Add in mainapp/models.py model PublicationCategory and model Publication

--- intergalactic/mainapp/models.py
+++ intergalactic/mainapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from authapp.models import IntergalacticUser
 
-# Create your models here.
+
+class PublicationCategory(models.Model):
+    name = models.CharField(verbose_name='имя категории', max_length=120)
+    desc = models.TextField(verbose_name='описание категории', blank=True)
+    created = models.DateTimeField(verbose_name='создана', auto_now_add=True)
+    is_active = models.BooleanField(db_index=True, default=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория публикации'
+        verbose_name_plural = 'категории публикаций'
+
+
+class Publication(models.Model):
+    category = models.ForeignKey(PublicationCategory, on_delete=models.CASCADE, verbose_name='категория публикации')
+    user = models.ForeignKey(IntergalacticUser, on_delete=models.CASCADE, verbose_name='автор публикации')
+    name = models.CharField('имя публикации', max_length=128)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    short_desc = models.CharField('краткое описание публикации', max_length=64, blank=True)
+    text = models.TextField('текст публикации', blank=True)
+    created = models.DateTimeField(verbose_name='создана', auto_now_add=True)
+    is_active = models.BooleanField(db_index=True, default=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'публикации'
