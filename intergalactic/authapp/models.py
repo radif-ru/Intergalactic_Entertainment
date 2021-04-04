311fe69790deb23b231c1d0f2a104d66275836a8
Radif <i@radif.ru>
2021-04-04 12:15:02 +0300

    Создать модель Users
    Создать таблицу users от модели AbstractUser.
    Содержит: (в скобках параметры данных для sqlite) (default что не нужно при регистрации, но нужно для профиля)
    - id (INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE)
    - nikname (VARCHAR (32))
    - name (VARCHAR (32) DEFAULT '')
    - surname (VARCHAR (32) DEFAULT '')
    - age (INTEGER NOT NULL DEFAULT 0)
    - email (VARCHAR (32) UNIQUE)
    - about (TEXT DEFAULT "")
    - company (VARCHAR (32) DEFAULT '')

--- intergalactic/authapp/models.py
+++ intergalactic/authapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Users(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    nikname = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, blank=True)
+    surname = models.CharField(max_length=32, blank=True)
+    age = models.PositiveIntegerField(verbose_name='возраст', null=False,
+                                      default=0)
+    email = models.EmailField(verbose_name='электронный адрес', unique=True)
+    about = models.TextField(blank=True)
+    company = models.CharField(max_length=32, blank=True)
