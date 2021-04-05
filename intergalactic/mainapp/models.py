c8b2e3dc694b58005cb43e361e41dfcbeef851a7
Dementev Oleg <demeoleg@mail.ru>
2021-04-05 11:21:06 +0300

    Changed the name of the folder for images of publications

--- intergalactic/mainapp/models.py
+++ intergalactic/mainapp/models.py
@@ -20,7 +20,7 @@ class Publication(models.Model):
     category = models.ForeignKey(PublicationCategory, on_delete=models.CASCADE, verbose_name='категория публикации')
     user = models.ForeignKey(IntergalacticUser, on_delete=models.CASCADE, verbose_name='автор публикации')
     name = models.CharField('имя публикации', max_length=128)
-    image = models.ImageField(upload_to='products_images', blank=True)
+    image = models.ImageField(upload_to='publications_images', blank=True)
     short_desc = models.CharField('краткое описание публикации', max_length=64, blank=True)
     text = models.TextField('текст публикации', blank=True)
     created = models.DateTimeField(verbose_name='создана', auto_now_add=True)
