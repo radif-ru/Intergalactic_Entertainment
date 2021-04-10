f28d2eade51140a8cdfe049d34e39e4fcb389f31
ShelpyakovAS <Shelpchel@gmail.com>
2021-04-11 03:51:55 +0500

    СПРЯТАЛ: 1) Главное меню - Поиск. 2) Второе меню под трендом. 3) Кнопка БОЛЬШЕ. 4) ПОПУЛЯРНЫЕ ЗА НЕДЕЛЮ. - Сделал как шаблон
    ДО РАБОТАТЬ:
    1) Шапка с социальными сетями это  ссылка на сети Intergalactic.
    2) Клик по Intergalactic добавить ссылку на главную.(если нету).
    3) СЕЙЧАС В ТРЕНДЕ: - Создать функцию определения тренда      топ 4 на основе Likes и Comments. (по лайкам и по дате)
    4) СЕЙЧАС В ТРЕНДЕ: - Добавить отображение трендов на страницу из БД. Добавить в тренды мини-блок Likes и Comments.
    5) БЫСТРЫЕ НОВОСТИ: - Изменить на СЕЙЧАС ЧИТАЮТ название.
    6)  БЫСТРЫЕ НОВОСТИ: - пересоздать в виде отдельного шаблона в includes.
    7) Футер - из ссылок пока сделать не Не ссылки.
    8) Для ПОСЛЕДНИЕ СТАТЬИ добавить отображение количество Likes и Comments из БД.

--- intergalactic/mainapp/views.py
+++ intergalactic/mainapp/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render, get_object_or_404
-from mainapp.models import Publication, PublicationCategory
+from mainapp.models import Publication, PublicationCategory, Likes, Comments
+from django.db.models import Count
+import operator
+
+
+def take_trendy_publication_id_list():
+    trendy_publication_id_counts = Likes.objects.values("publication_id").annotate(Count("id"))
+    trendy_publication_id_counts = sorted(trendy_publication_id_counts, key=lambda x: x['id__count'], reverse=True)
+    id_list = []
+    for vocabulary in trendy_publication_id_counts:
+        id_list.append(vocabulary["publication_id"])
+    return id_list
+
+
+def take_publications_list_of_dict(publications_list):
+    publications_list_of_dict = []
+    for publication in publications_list:
+        likes = Likes.objects.filter(publication_id=publication).count()
+        comments = Comments.objects.filter(publication=publication).count()
+        publications_list_of_dict.append({"publication": publication, "likes": likes, "comments": comments})
+    return publications_list_of_dict
 
 
 def main(request):
     categories = PublicationCategory.objects.filter(is_active=True)
-    publication_list =Publication.objects.filter(is_active=True, category__is_active=True).order_by('-created')
+
+    publication_list = Publication.objects.filter(is_active=True, category__is_active=True).order_by('-created')
+    publication_list_of_dict = take_publications_list_of_dict(publication_list)
+
+    trendy_id_list = take_trendy_publication_id_list()
+    trendy_publication_list = [
+        Publication.objects.get(id=trendy_id_list[0]),
+        Publication.objects.get(id=trendy_id_list[1]),
+        Publication.objects.get(id=trendy_id_list[2]),
+        Publication.objects.get(id=trendy_id_list[3])
+    ]
+    trendy_publication_list_of_dict = take_publications_list_of_dict(trendy_publication_list)
+
     content = {
         'categories': categories,
         'title': 'главная',
-        'publication_list': publication_list,
+        'publication_list_of_dict': publication_list_of_dict,
+        'trendy_publication_list_of_dict': trendy_publication_list_of_dict
     }
     return render(request, 'mainapp/index.html', content)
 
